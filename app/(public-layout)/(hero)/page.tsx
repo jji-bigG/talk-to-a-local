58b247e4b049504bf99e03e38c20251a520e0408
ruiyang.ji1 <ruiyang.ji1@leyantech.com>
2024-06-21 14:31:57 +0800

    init

--- app/(public-layout)/(hero)/page.tsx
+++ app/(public-layout)/(hero)/page.tsx
@@ -5,14 +5,11 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center gap-5 justify-center h-[70vh]">
       <h3 className="text-4xl font-bold text-center">
-        Welcome to OpenChat! Your Gateway to Intelligent Conversations, Powered
-        by OpenAPI
+        Talk to a Local! <br /> Explore a new community, town, or city.
       </h3>
       <p className="sm:w-[75%] mx-auto text-center text-muted-foreground ">
-        OpenChat redefines the chat experience by seamlessly integrating the
-        robust capabilities of the OpenAPI. This platform takes conversational
-        AI to new heights, offering you a dynamic and intelligent chat companion
-        designed for a wide array of applications.
+        We find the best local guides to integrate to our LLM, built using the latest
+        LLM technology. You provide the LLM and we provide the guide.
       </p>
       <Link href="/register" className={buttonVariants({ size: "lg" })}>
         Get started
